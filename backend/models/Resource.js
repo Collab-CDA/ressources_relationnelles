0c734338021b95e564539c630a749f5831678fce
wattsans <wattsans@gmail.com>
2025-02-02 12:45:38 +0100

    ressource

--- backend/models/Resource.js
+++ backend/models/Resource.js
@@ -1,12 +1,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db/sequelize');
 
-const Resource = sequelize.define('Ressource', {
-  titre: { 
+const Resource = sequelize.define('Resource', {
+  nom: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true,
   },
-  contenu: { 
+  description: {
     type: DataTypes.TEXT,
     allowNull: true
   },
@@ -14,32 +15,40 @@ const Resource = sequelize.define('Ressource', {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      isIn: [[
-        'Activité / Jeu à réaliser',
-        'Article',
-        'Carte défi',
-        'Cours au format PDF',
-        'Exercices / Atelier',
-        'Fiche de lecture',
-        'Jeu en ligne',
-        'Vidéo'
-      ]] 
+      isIn: [['text', 'pdf', 'video', 'image']] 
     }
   },
   url: {
     type: DataTypes.STRING,
     allowNull: true,
     validate: {
-      isUrl: true // Valide l'URL si fournie
+      isUrl: true, 
+      is: /^(https?:\/\/)[^\s$.?#].[^\s]*$/i
     }
   },
   valide: {
     type: DataTypes.BOOLEAN,
     defaultValue: false
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'categories',
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
   }
 }, {
   timestamps: true,
-  tableName: 'ressources'
+  tableName: 'ressources',
+  indexes: [
+    {
+      unique: true,
+      fields: ['nom']
+    }
+  ]
 });
 
 module.exports = Resource;
